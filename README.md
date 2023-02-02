ad664dc35f838b06ad6ca58efbf683c227a99cf1
Dan Buchholz <dbuchholz30@gmail.com>
2023-02-02 11:31:49 -0500

    feat: update comments, README

--- README.md
+++ README.md
@@ -1,46 +1,44 @@
-# Getting Started with Create React App
+# Getting started with Tableland
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+A basic React app with a browser wallet (or local private key) connection to Tableland while using the SDK.
 
-## Available Scripts
+## Background
 
-In the project directory, you can run:
+### Overview
 
-### `npm start`
+This tutorial walks through the basics of using the [Tableland SDK](https://github.com/tablelandnetwork/js-tableland). It creates a very basic [React](https://reactjs.org/) app where a user will first connect their wallet. Then, they'll be able to create a table (minted to their address), write to the table, and read data from the table. The table's name and table data get rendered in the UI, and there are some special SQL features demonstrated as well. Lastly, the [`local-tableland`](https://github.com/tablelandnetwork/local-tableland) is leveraged to make it easy to develop locally with both a Tableland and [Hardhat](https://hardhat.org/) node.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+One of the key pieces to understand is the `Database` API. Every query in Tableland runs through the same API, where you can pass create statements, writes, and reads to `Database.prepare(...)` and then execute them. An execution should come with a account singer for on-chain transactions (create, writes) but need not be passed to `Database` for reads. Each query execution comes with a `metadata`, `results`, and `success` in its response. Only read queries make use of `results`, but all queries will have `metadata` and `success` returned.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+### Project structure
 
-### `npm test`
+The table schema is hardcoded as the following: `id integer primary key, name text, block text, tx text`. What's important to understand is that
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+- `id integer primary key` will auto-increment upon inserting values into the table but not defining the `id` column. In other words, if you `INSERT INTO my_table_name (name) VALUES ("Bobby Tables")`, the `id` column will increment will each new row inserted.
+- The `block` column will have the chain's block number inserted by using the "magic" function `BLOCK_NUM()` within an insert statement (e.g., `INSERT INTO my_table_name (block) VALUES (BLOCK_NUM())`).
+- Similarly, this is used for the `tx` (transaction hash) column with the function `TXN_HASH()`.
+  Once a table is created and written to, the table data can be displayed in the UI by clicking the "Read" button. There are more sophisticated ways to do this in a production setting, like polling for table changes, but the core read functionality is demonstrated appropriately
 
-### `npm run build`
+For local development, the `local-tableland` package is installed, making it easy to run these changes while connecting to a local hardhat node. Thus, you can test things out locally on both a chain and the Tableland network.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+This app is pretty straightforward and uses the boilerplate code from `npx create-react-app`, and it uses the TypeScript template. For those looking to use a private key connection instead of a browser wallet, there is some commented out code in the `connect()` method that allows you to read a private key from `.env` to create a `Wallet`. All in all, the following methods are implemented:
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- `connect()`: Make an account connection to later use a signer for Tableland `Database` API interactions.
+- `create()`: Create a table with the connected signer, using a form input value for the table's `prefix`.
+- `write()`: Write to the table with form input value; this only mutates a single `name` column, and the others (`id`, `block`, and `tx`) are automatically updated by the database using auto-increment functionality or magic functions.
+- `read()`: Use a read-only `Database` connection (i.e., you don't need a signer to read) to retrieve table data.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+All application code is placed in `App.tsx` to keep things simple, and some basic styles are included in `App.css`. ANd for more implementation details, comments are also provided throughout the code.
 
-### `npm run eject`
+## Usage
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+First, install all dependencies with `npm install`. Then, in the project directory, you should:
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+1. Start a local Tableland node (available at [http://localhost:8080](http://localhost:8080)): `npx local-tableland`
+2. In a separate window, start the React app (opens in [http://localhost:3000](http://localhost:3000)): `npm start`
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+If you're interested in directly querying the local Tableland node, you can also make API requests directly in your browser to [http://localhost:8080](http://localhost:8080), such as the auto-generated healthbot table or the table you create in the app itself. Just pass a read query to the `/query` endpoint, such as `SELECT * FROM healthbot_31337_1`:
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+```
+http://localhost:8080/query?s=select%20*%20from%20healthbot_31337_1
+```
--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import "./App.css";
 import { Wallet, getDefaultProvider, Signer, providers } from "ethers";
 import { Database, Validator } from "@tableland/sdk";
 
-// Provide a global `window` variable to (naively) prevent type errors
+// Provide a global `window` variable to (naively) prevent type errors when
+// connecting to a browser wallet.
 declare const window: any;
 
-// Define a table schema——e.g., this is used if you want to strongly type `Database`
+// Define a table schema——e.g., used if you want to strongly type `Database`.
 interface TableSchema {
   id: number;
   name: string;
@@ -14,54 +15,79 @@ interface TableSchema {
   tx: string;
 }
 
-// Create a connection with the base chain and return the `Signer`
+/**
+ * Create a connection with the base chain and return the `Signer`.
+ *
+ * @returns A `Signer`—the signing account (though a browser wallet connection
+ * or private key).
+ */
 async function connect(): Promise<Signer | undefined> {
   /**
-   * Connecting a signer using a private key
+   * Connecting a signer using a browser wallet connection.
    *
-   * For a local wallet connection, create a signer using a private key stored in `.env`
-   * Note that `Wallet` is extension of `Signer`. `Wallet` initializes with a private key,
-   * whereas `Signer` does not (signs a message). `Wallet` has a few other methods available.
+   * For a browser wallet connections, prompt the user to sign the message. This
+   * is the default behavior with `new Database()`, but for demonstration
+   * purposes, an instance of `Signer` is passed to the database for table
+   * creates & writes.
    *
-   * The code below is commented out, but if you'd like to connect using a private key,
-   * uncomment this and delete the code under "Connecting a signer using a browser wallet connection"
+   * If you'd like to connect using a private key, delete the code below and
+   * uncomment the section below under "Connecting a signer using a private key".
    */
-  // // Import the private key, made available in React using the `REACT_APP_` prefix
-  // const privateKey = process.env.REACT_APP_PRIVATE_KEY;
-  // // Define the signer and connect to the provider
-  // let signer;
-  // if (privateKey !== undefined) {
-  //   const wallet = new Wallet(privateKey);
-  //   const provider = getDefaultProvider("http://127.0.0.1:8545"); // A local Hardhat node from running `npx local-tableland`
-  //   signer = wallet.connect(provider);
-  // }
-
-  /**
-   * Connecting a signer using a browser wallet connection
-   *
-   * For a browser wallet connections, prompt the user to sign the message. This is
-   * the default behavior with `new Database()`, but for demonstration purposes,
-   * an instance of `Signer` is passed to the database for table creates & writes.
-   *
-   * If you'd like to connect using a private key, delete the code below and uncomment
-   * the section above under "Connecting a signer using a private key"
-   */
-  // Establish a connection with the browser wallet's provider
+  // Establish a connection with the browser wallet's provider.
   const provider = new providers.Web3Provider(window.ethereum);
-  // Request the connected accounts, prompting a browser wallet popup to connect
+  // Request the connected accounts, prompting a browser wallet popup to connect.
   await provider.send("eth_requestAccounts", []);
-  // Create a signer from the returned provider connection
+  // Create a signer from the returned provider connection.
   const signer = provider.getSigner();
   // Return the signer
   return signer;
+
+  /**
+   * Connecting a signer using a private key.
+   *
+   * For a local wallet connection, create a signer using a private key stored
+   * in `.env`. Note that `Wallet` is extension of `Signer`. `Wallet`
+   * initializes with a private key, whereas `Signer` does not (signs a
+   * message). `Wallet` has a few other methods available.
+   *
+   * The code below is commented out, but if you'd like to connect using a
+   * private key, uncomment this and delete the code above for "Connecting a
+   * signer using a browser wallet connection".
+   */
+  // // Import the private key, available in `.env. using `REACT_APP_` prefix.
+  // const privateKey = process.env.REACT_APP_PRIVATE_KEY;
+  // // Define the signer and connect to the provider.
+  // let signer;
+  // if (privateKey !== undefined) {
+  //   const wallet = new Wallet(privateKey);
+  // // A local Hardhat node from running `npx local-tableland`, but replace
+  // // with any provider URL (e.g., Alchemy, Infura, Etherscan, etc.).
+  //   const provider = getDefaultProvider("http://127.0.0.1:8545");
+  //   signer = wallet.connect(provider);
+  // }
 }
 
-// Create a table with a `prefix` value from a form input
+/**
+ * Create a table with a `prefix` value from a form input.
+ *
+ * @param signer The signing account for all transactions.
+ * @param prefix A human readable table identifier (as part of a form input).
+ * @returns Auto-generated table name, in the format `{prefix}_{chainId}_{tableId}`
+ */
 async function create(
   signer: Signer,
   prefix: string
 ): Promise<string | undefined> {
+  // Establish a connection with the database and define the expected type.
   const db = new Database<TableSchema>({ signer });
+  /**
+   * Assign the `meta` values to `create` (metadata about the create action,
+   * like duration, table name, etc.). Note: other response keys include
+   * `success` (if the tx succeeded) and `results` (only for read queries,
+   * otherwise, empty). Also note the schema is hardcoded as the following:
+   *    id integer primary key, name text, block text, tx text
+   * The `id integer primary key` can auto-increment when no value provided.
+   */
   const { meta: create } = await db
     .prepare(
       `CREATE TABLE "${prefix}" (id integer primary key, name text, block text, tx text);`
@@ -70,44 +96,89 @@ async function create(
   return create.txn?.name;
 }
 
-// Write to the table with a `name` value from a form input
+/**
+ * Write to the table with a `name` value from a form input.
+ *
+ * @param signer The signing account for all transactions.
+ * @param tableName Generated table name in the format
+ * `{prefix}_{chainId}_{tableId}`.
+ * @param name Table value to insert into the `name` column.
+ * @returns Boolean value for if the write tx was successful.
+ */
 async function write(signer: Signer, tableName: string, name: string) {
+  // Establish a connection with the database and define the expected type.
   const db = new Database<TableSchema>({ signer });
+  // Assign the `meta` values to `write` (metadata about the write action, such
+  // as tx hash). Notice the "magic" functions to automatically grab the chain's
+  // `BLOCK_NUM()` and `TXN_HASH()`.
   const { success, meta: insert } = await db
     .prepare(
       `INSERT INTO ${tableName} (name, block, tx) VALUES (?, BLOCK_NUM(), TXN_HASH());`
     )
     .bind(name)
     .run();
+  // Await the write tx's success.
   await insert.txn?.wait();
+  // Upon a submitted transaction, the `success` field in the response object
+  // will return a boolean.
   return success;
 }
 
-// Read from the created & mutated table on the selected chain
+/**
+ * Read from the created & mutated table on the selected chain.
+ *
+ * @param signer The signing account for all transactions (but only needed to
+ * get the chain ID).
+ * @param tableName Generated table name in the format
+ * `{prefix}_{chainId}_{tableId}`.
+ * @returns Table values, which are objects in an array.
+ */
 async function read(signer: Signer, tableName: string): Promise<TableSchema[]> {
+  // Grab the chain ID from the connected signer.
   const chainId = await signer.getChainId();
-  console.log(chainId);
+  // Create a read-only connection to a single chain, where the `Database`
+  // doesn't need a signer.
   const db = Database.readOnly(chainId);
+  // Fetch the table data——the results are an array of table data (recall that
+  // creates and writes have empty results).
   const { results } = await db
     .prepare(`SELECT * FROM ${tableName};`)
     .all<TableSchema>();
+  // Return the array of results (a series of `TableSchema` objects).
   return results;
 }
 
-// A basic app with form inputs and table rendering
+/**
+ * A basic app with form inputs and table rendering.
+ *
+ * The setup is rather basic and not necessarily "best practice" for React, but
+ * it gets the job done. A `signer` is saved to state upon an initial wallet
+ * connection, along with the wallet's `address`. Then, a table is created where
+ * its name is saved to `tableName`.
+ *
+ * Upon writing to a table, the success of the write tx is saved to
+ * `writeSuccess`. This value is used for demo purposes with the `success`
+ * Database response and used to disable the "Read" query button until data is
+ * written to a table. Lastly, upon manually clicking a "Read" query button, the
+ * table's data is saved to `data` and then rendered in a table within the UI.
+ */
 function App() {
-  // App data
+  // Signer & associated account address data
   const [signer, setSigner] = useState<Signer | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [address, setAddress] = useState<string>("");
+  // Table related data upon creating, writing to, and reading from tables.
   const [tableName, setTableName] = useState<string | undefined>(undefined);
   const [writeSuccess, setWriteSuccess] = useState<boolean>(false);
   const [data, setData] = useState<TableSchema[]>([]);
-  // Form data
+  // Form related data that gets passed to Tableland `Database.prepare()`
+  // methods.
   const [prefix, setPrefix] = useState<string>("");
   const [writeData, setWriteData] = useState<string>("");
+  // A naive way of displaying a loader (imperfect w/ browser wallet rejects /
+  // cancels.
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  // Handle form input data change
+  // Handle form input data changes for the table's `prefix` and `write` data.
   function handleChange(e: any) {
     switch (e.target.name) {
       case "prefix":
@@ -123,11 +194,14 @@ function App() {
 
   return (
     <>
-      {/* Basic navbar with wallet `Connect` button */}
+      {
+        // Basic navbar with a wallet `Connect` button.
+      }
       <nav>
         <h1>Getting started with Tableland</h1>
         {
-          // If there is a signer established, render the button with the signer's address
+          // If there is a signer established, render the button with the
+          // signer's address. Else, show "Connect".
           <div>
             <button
               type="button"
@@ -146,7 +220,7 @@ function App() {
       <div className="container">
         <h2>Interact with the database</h2>
         {
-          // Once a signer is set via `connect()`, enable the create button
+          // Once a signer is set via `connect()`, enable the create button.
           <>
             <form>
               <input
@@ -173,7 +247,7 @@ function App() {
           </>
         }
         {
-          // Once a table name is set via `create()`, enable the write button
+          // Once a table name is set via `create()`, enable the write button.
           <>
             <form>
               <input
@@ -203,10 +277,13 @@ function App() {
             </h5>
             <h3>Table name</h3>
             {
-              // Display the table name after it is created with `create()`
+              // Display the table name after it is created with `create()`.
               !tableName ? "No table created, yet." : <p>{tableName}</p>
             }
-            {/* Once data is written via `write()`, read from the table and set the app's state `data` */}
+            {
+              // Once data is written via `write()`, read from the table and set
+              // `data`.
+            }
             <div>
               <h3>↓ Read from the table</h3>
               <button
@@ -228,7 +305,8 @@ function App() {
         }
         <h2>Table data</h2>
         {
-          // Render the table data upon making `read()` calls from the read button
+          // Render the table data upon making `read()` calls by manually
+          // clicking the read button (you could imagine polling, instead).
           data.length === 0 ? (
             <p>
               No data is written, yet—or you haven't clicked <b>Read</b>.
@@ -257,7 +335,10 @@ function App() {
           )
         }
       </div>
-      {/* Display a loading overlay upon various method calls (note: imperfect logic is `isLoading`) */}
+      {
+        // Display a loading overlay upon various method calls (note: imperfect
+        // logic in `isLoading`; wallet rejects will keep the spinner going).
+      }
       {isLoading && <div id="loading"></div>}
     </>
   );
