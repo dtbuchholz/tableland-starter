87fc9e850dc81a0108175813eb23034f4c598cc2
Dan Buchholz <dbuchholz30@gmail.com>
2023-02-03 17:30:54 -0800

    refactor: cleanups, docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect, useState, useCallback } from "react";
-import "./App.css";
+import { useEffect, useState, useCallback } from "react";
+// For private key connections, you'll need `Wallet` & `getDefaultProvider`.
+// For browser wallet connections, only `Signer` & `providers` are needed.
 import { Wallet, getDefaultProvider, Signer, providers } from "ethers";
 import { Database, Validator, Registry, helpers } from "@tableland/sdk";
+import "./App.css";
+
+// Note: This tutorial does not necessarily exhibit best practices with but
+// demonstrates how to use the Tableland SDK and its various APIs.
 
 // Provide a global `window` variable to (naively) prevent type errors when
 // connecting to a browser wallet.
@@ -18,14 +23,13 @@ interface TableSchema {
 /**
  * Create a connection with the base chain and return the `Signer`.
  *
- * @returns A `Signer`—the signing account (though a browser wallet connection
- * or private key).
+ * Using other popular libraries (e.g., wagmi) can be used, instead, so that you
+ * don't have to implement logic like this yourself.
+ *
+ * @returns The signing account (though a browser wallet connection or private
+ * key).
  */
-async function connect(): Promise<{
-  signer: Signer;
-  db: Database;
-  validator: Validator;
-}> {
+async function connect(): Promise<Signer> {
   // Connecting a signer using a browser wallet connection.
   //
   // For a browser wallet connections, prompt the user to sign the message. This
@@ -42,13 +46,8 @@ async function connect(): Promise<{
   await provider.send("eth_requestAccounts", []);
   // Create a signer from the returned provider connection.
   const signer = provider.getSigner();
-  // Connect to the Tableland database, where the chain ID defines the base URL.
-  const chainId = await signer.getChainId();
-  const db = new Database({ signer, baseUrl: helpers.getBaseUrl(chainId) });
-  // Connect to a Tableland validator node (used for some specific APIs)
-  const validator = new Validator(db.config);
   // Return the signer
-  return { signer, db, validator };
+  return signer;
 
   //  Connecting a signer using a private key.
   //
@@ -69,13 +68,8 @@ async function connect(): Promise<{
   // // with any provider URL (e.g., Alchemy, Infura, Etherscan, etc.).
   // const provider = getDefaultProvider("http://127.0.0.1:8545");
   // const signer = wallet.connect(provider);
-  // // Connect to the Tableland database, where the chain ID defines the base URL.
-  // const chainId = await signer.getChainId();
-  // const db = new Database({ signer, baseUrl: helpers.getBaseUrl(chainId) });
-  // // Connect to a Tableland validator node (used for some specific APIs)
-  // const validator = new Validator(db.config);
   // // Return the signer
-  // return { signer, db, validator };
+  // return signer;
 }
 
 /**
@@ -83,7 +77,8 @@ async function connect(): Promise<{
  *
  * @param signer The signing account for all transactions.
  * @param prefix A human readable table identifier (as part of a form input).
- * @returns Auto-generated table name, in the format `{prefix}_{chainId}_{tableId}`
+ * @returns Auto-generated table name, in the format
+ * `{prefix}_{chainId}_{tableId}`, as well as the table schema.
  */
 async function create(
   signer: Signer,
@@ -92,14 +87,14 @@ async function create(
   tableName: string;
   schema: string[];
 }> {
-  // Establish a connection with the database and define the expected type.
-  // Here, we'll be doing some extra setup to then use the `Validator` API down
-  // below. To do so, we'll need to pass a `baseUrl` that allows us to connect
-  // directly to the Tableland network (validator node) and look up table info.
-  // The first step is to grab the `chainId` and then use the helper method to
-  // connect to either a Tableland local, testnets, or mainnets node.
+  // Establish a connection with the database. Here, we'll be doing some extra
+  // setup to then use the `Validator` API down below. To do so, we'll need to
+  // pass a `baseUrl` that allows us to connect directly to the Tableland
+  // network (validator node) and look up table info. The first step is to grab
+  // the `chainId` and then use the `helpers` method to connect to either a
+  // Tableland local, testnets, or mainnets node.
   const chainId = await signer.getChainId();
-  const db = new Database<TableSchema>({
+  const db = new Database({
     signer,
     baseUrl: helpers.getBaseUrl(chainId),
   });
@@ -108,29 +103,31 @@ async function create(
   // `success` (if the tx succeeded) and `results` (only for read queries,
   // otherwise, empty). Also note the schema is hardcoded as the following:
   //    id integer primary key, name text, block text, tx text
-  // The `id integer primary key` can auto-increment when no value provided.
+  // The `id integer primary key` will auto-increment when no value is provided.
   const { meta: create } = await db
     .prepare<TableSchema>(
       `CREATE TABLE "${prefix}" (id integer primary key, name text, block text, tx text);`
     )
     .run();
+  // Call `wait()` to await the tx to complete, then, grab the table's
+  // auto-generated name (`{prefix}_{chainId}_{tableId}`) and ID.
   await create.txn?.wait();
-  const tableName = create.txn!.name;
-  // Get the created table ID from the create transaction.
-  const { tableId } = create.txn!;
-  // Now, create a connection to a Tableland `Validator` on the chain ID to look
-  // up table information, such as the table's schema. (Pretend we don't already
-  // know it and need to look it up.)
+  const { tableId, name: tableName } = create.txn!;
+  // Next, we'll demonstrate how to use the Tableland SDK's validator API.
+  // Create a connection to a Tableland `Validator` on the chain ID to look up
+  // table information, such as the table's schema. (Pretend we don't already
+  // know the schema and need to look it up.)
   const validator = new Validator(db.config);
   const tableInfo = await validator.getTableById({
     chainId,
     tableId,
   });
-  // Table's schema is an object with keys name, type, and constraints. Convert
-  // this into a single string for each column.
+  // A table's `schema` is an object with keys `name`, `type`, and
+  // `constraints`. Convert this object into a single string for each column.
   const schema = tableInfo.schema.columns.map((col) => {
     return `${col.name} ${col.type} ${col.constraints ? col.constraints : ""}`;
   });
+  // Return the table name and its per-column schema.
   return { tableName, schema };
 }
 
@@ -141,9 +138,13 @@ async function create(
  * @param tableName Generated table name in the format
  * `{prefix}_{chainId}_{tableId}`.
  * @param name Table value to insert into the `name` column.
- * @returns Boolean value for if the write tx was successful.
+ * @returns A pending write query transaction hash.
  */
-async function write(signer: Signer, tableName: string, name: string) {
+async function write(
+  signer: Signer,
+  tableName: string,
+  name: string
+): Promise<string> {
   // Establish a connection with the database and define the expected type.
   const db = new Database<TableSchema>({ signer });
   // Assign the `meta` values to `write` (metadata about the write action, such
@@ -156,14 +157,13 @@ async function write(signer: Signer, tableName: string, name: string) {
     .bind(name)
     .run();
   // Return the (pending) tx hash
-  return insert.txn?.transactionHash;
+  return insert.txn!.transactionHash;
 }
 
 /**
  * Read from the created & mutated table on the selected chain.
  *
- * @param signer The signing account for all transactions (but only needed to
- * get the chain ID).
+ * @param chainId The chain ID of the connected chain.
  * @param tableName Generated table name in the format
  * `{prefix}_{chainId}_{tableId}`.
  * @returns Table values, which are objects in an array.
@@ -187,41 +187,51 @@ async function read(
 /**
  * A basic app with form inputs and table rendering.
  *
- * The setup is rather basic and not necessarily "best practice" for React, but
+ * The setup is rather simple & not necessarily "best practices" for React, but
  * it gets the job done. A `signer` is saved to state upon an initial wallet
- * connection, along with the wallet's `address`. Then, a table is created where
- * its name is saved to `tableName`.
+ * connection, along with the wallet's `address` & `chainId`. Then, a table is
+ * created where its name & schema are saved to `tableName` & `schema`.
  *
- * Upon writing to a table, the success of the write tx is saved to
- * `writeSuccess`. This value is used for demo purposes with the `success`
- * Database response and used to disable the "Read" query button until data is
- * written to a table. Lastly, upon manually clicking a "Read" query button, the
- * table's data is saved to `data` and then rendered in a table within the UI.
+ * Upon writing to a table, the tx hash of write query is saved to
+ * `pendingWriteTx`. This value is used as part of the polling process to read
+ * table data and is also used to disable the "Read" query button until data is
+ * written to a table. Various info (`rowCount` & `tablesOwnedCount`) is also
+ * fetched for demo purposes with read queries and also using the Registry API.
+ * There are also a couple of form-related state is also tracked when a user
+ * sets the table `prefix` or `writeData`.
+ *
+ * A few callback hooks are used to set / unset the pending write tx, fetch (or
+ * reset) table data, or retrieve a count of total owned tables. There's an
+ * interesting `useEffect` with polling for a tx to finalize
+ * (`pollForReceiptByTransactionHash`). Lastly, a number of handlers are used
+ * to help with form data changes / submits.
  */
 function App() {
-  // Signer, its associated account address data, and the connect chain ID.
+  // Track the signer——but note that using external libraries & hooks (e.g.,
+  // wagmi) can help circumvent needing to implement your own wallet logic.
   const [signer, setSigner] = useState<Signer>();
-  const [db, setDb] = useState<Database>();
-  const [validator, setValidator] = useState<Validator>();
+  // Use the address for a misc. UI feature, and chain ID helps with connecting
+  // to Tableland.
   const [address, setAddress] = useState<string>();
   const [chainId, setChainId] = useState<number>();
-  // Table related data upon creating, writing to, and reading from tables.
+  // Table (or general account) related data upon creating, writing to, and
+  // reading from tables.
   const [tableName, setTableName] = useState<string>();
   const [schema, setSchema] = useState<string[]>([]);
   const [data, setData] = useState<TableSchema[]>([]);
   const [rowCount, setRowCount] = useState<number>();
   const [tablesOwnedCount, setTablesOwnedCount] = useState<number>();
   // Form related data that gets passed to Tableland `Database.prepare()`
-  // methods. The prefix & write data *can* be an empty string.
+  // methods. The prefix & write data *can* each be an empty string.
   const [prefix, setPrefix] = useState<string>("");
   const [writeData, setWriteData] = useState<string>("");
   // A naive way of displaying a loader (imperfect w/ browser wallet rejects /
-  // cancels.
+  // cancels) using `isLoading`; `pendingWriteTx` will handle wallet rejects.
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [pendingWriteTx, setPendingWriteTx] = useState<string>();
 
   // When a write tx is made, the `pendingWriteTx` is set. This resets the tx
-  // and also trigger a read query to update the `data`, rendered in the UI.
+  // and also triggers a read query to update the `data`, rendered in the UI.
   const clearPendingTxAndRefresh = useCallback(async () => {
     setPendingWriteTx(undefined);
     const data = await read(chainId!, tableName!);
@@ -234,29 +244,47 @@ function App() {
     setRowCount(undefined);
   }, [tableName]);
 
+  // Upon the signer connecting, get a count of all owned tables and continually
+  // update the count wih new table creates.
   const getOwnedTables = useCallback(async () => {
-    if (db) {
-      // Connect to the registry smart contract and look up owned tables by address.
+    if (signer) {
+      // Connect to the registry smart contract and look up owned tables by
+      // singer address.
+      const db = new Database({
+        signer,
+        baseUrl: helpers.getBaseUrl(chainId!),
+      });
       const registry = new Registry(db.config);
+      // Call `listTables()` that retrieves some aggregated ownership data from
+      // the registry contract.
       const ownedTables = await registry.listTables(); // Default will use connected account.
+      // Filer down `ownedTables`, which contains table IDs and its chain ID.
       const tableIds = ownedTables.map((t) => t.tableId);
       setTablesOwnedCount(tableIds.length);
     }
   }, [signer]);
 
-  // Use the `resetTableData` callback to reset the table data displayed.
+  // Use the `resetTableData` callback to reset the table data displayed as well
+  // as update the count of owned tables with `getOwnedTables`.
   useEffect(() => {
     resetTableData();
     getOwnedTables();
   }, [resetTableData, getOwnedTables]);
 
-  // Upon pending write transactions (set by `handleWrite` & `write`), poll the
-  // tx receipt. Once the validator confirms the receipt, that means the write
-  // tx was seen by the Tableland validator. Thus, if the write tx was
+  // Upon pending write transactions (set by `handleWrite` & `write`), poll for
+  // the tx receipt. Once the validator confirms the receipt, that means the
+  // write tx was seen by the Tableland validator. Thus, if the write tx was
   // successful, then the table data has changed, which is a nice trigger to
   // then go fetch new table data and update state accordingly.
   useEffect(() => {
-    if (validator && pendingWriteTx) {
+    if (pendingWriteTx) {
+      // Connect to the Tableland database, where the chain ID defines the base URL.
+      const db = new Database({
+        signer,
+        baseUrl: helpers.getBaseUrl(chainId!),
+      });
+      // Connect to a Tableland validator node (used for some specific APIs)
+      const validator = new Validator(db.config);
       // Create a controller & signal to help abort the pending tx request, once
       // it is fulfilled.
       const controller = new AbortController();
@@ -281,9 +309,22 @@ function App() {
         controller.abort();
       };
     }
-  }, [pendingWriteTx, clearPendingTxAndRefresh]);
+  }, [chainId, pendingWriteTx, clearPendingTxAndRefresh]);
 
-  // Handle form input data changes for the table's `prefix` and `write` data.
+  // Handle click for connecting an account and setting the `signer`,
+  // using that to set the `address` and `chainId`. These are used
+  // throughout various methods, particularly, the `Database`.
+  async function handleConnect() {
+    // Connect a signer
+    const signer = await connect();
+    const addr = await signer!.getAddress();
+    const chainId = await signer.getChainId();
+    setSigner(signer);
+    setAddress(addr);
+    setChainId(chainId);
+  }
+
+  // Handle form input data changes for the table's `prefix` and `writeData`.
   function handleChange(e: any) {
     switch (e.target.name) {
       case "prefix":
@@ -297,24 +338,10 @@ function App() {
     }
   }
 
-  // Handle click for connecting an account and setting the `signer`,
-  // `database`, `validator`, `address`, and `chainId`. These are used
-  // throughout various methods, particularly, the `Database`.
-  async function handleConnect() {
-    const { signer, db, validator } = await connect();
-    const addr = await signer!.getAddress();
-    const chainId = await signer!.getChainId();
-    setSigner(signer);
-    setAddress(addr);
-    setChainId(chainId);
-    setDb(db);
-    setValidator(validator);
-  }
-
   // Handle click for creating a table, which captures the generated `tableName`
   // and then fetches the table's schema directly from a Tableland validator.
   // The loading is rather naive but demonstrates a different way of achieving
-  // this, as opposed to the `handleWrite` below & `setPendingWriteTx`.
+  // this, as opposed to the `handleWrite` type of logic w/ `setPendingWriteTx`.
   async function handleCreate(e: any) {
     e.preventDefault();
     setIsLoading(true);
@@ -323,8 +350,6 @@ function App() {
         // Create a table
         const { tableName, schema } = await create(signer, prefix);
         setTableName(tableName);
-        // setOwnedTables(ownedTables);
-
         setSchema(schema);
       } catch {
         setIsLoading(false);
@@ -339,14 +364,17 @@ function App() {
   // fetched, and the `pendingWriteTx` will be cleared.
   async function handleWrite(e: any) {
     e.preventDefault();
+    // Make a write query
     const tx = await write(signer!, tableName!, writeData);
     setPendingWriteTx(tx);
   }
 
-  // Handle click for reading from the table, which updates the `data` that is
-  // then rendered in the UI's table.
+  // Handle click for reading from the table, which updates the `rowCount`. This
+  // is demonstrated for simple "manual" fetch vs. the polling option shown
+  // above with the validator's `pollForReceiptByTransactionHash`.
   async function handleRead() {
     setIsLoading(true);
+    // Read table data
     const data = await read(chainId!, tableName!);
     setRowCount(data.length);
     setIsLoading(false);
@@ -471,7 +499,7 @@ function App() {
               <p>
                 <b># of created tables: </b>
                 {
-                  // Fetch & show the row count once a user clicks "Read".
+                  // Fetch & update the # of owned tables after a signer connects.
                   !tablesOwnedCount ? "No owned tables." : `${tablesOwnedCount}`
                 }
               </p>
@@ -483,7 +511,9 @@ function App() {
           {
             // Render the table data upon pending write txs being finalized.
             // This happens by way of polling for tx confirmation.
-            data.length > 0 ? (
+            data.length === 0 ? (
+              <p>No data is written to the table.</p>
+            ) : (
               <table>
                 <thead>
                   <tr>
@@ -493,6 +523,11 @@ function App() {
                     <th>transaction hash</th>
                   </tr>
                 </thead>
+                {
+                  // Note the usage of `getBaseUrl`. This helper will create the
+                  // base URL at the validator, allowing you to leverage
+                  // something like a Validator REST API that points to a tx.
+                }
                 <tbody>
                   {data.map((d) => (
                     <tr key={d.id}>
@@ -514,15 +549,14 @@ function App() {
                   ))}
                 </tbody>
               </table>
-            ) : (
-              <p>No data is written to the table.</p>
             )
           }
         </div>
       </div>
       {
-        // Display a loading overlay upon various method calls (note: imperfect
-        // logic in `isLoading`; wallet rejects will keep the spinner going).
+        // Display a loading overlay upon various method calls, where
+        // `isLoading` is used by creates & reads, and `pendingWriteTx` is used
+        // when table write queries are made.
       }
       {(pendingWriteTx || isLoading) && <div id="loading"></div>}
     </>
